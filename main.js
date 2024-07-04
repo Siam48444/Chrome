1e45376f3db5615284a13c751bdc6de6ea4aaac7
Siam <siam48444@gmail.com>
2024-07-04 18:47:25 +0600

    main

---
+++ main.js
@@ -0,0 +1,15 @@
+old_Y = window.scrollY;
+function nav_scroll_direction_change() {
+    function scrolling_down() {
+        gsap.to("nav", { y: "-105%", duration: 2, ease: "expo.out" });
+    }
+    function scrolling_up() {
+        gsap.to("nav", { y: 0, duration: 2, ease: "expo.out" });
+    }
+
+    old_Y < window.scrollY ? scrolling_down() : scrolling_up();
+    old_Y = window.scrollY;
+}
+window.addEventListener("scroll", () => {
+    nav_scroll_direction_change();
+});
