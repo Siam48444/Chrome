a6e38f7b1b5ddfaae9b36570d6d44aa0dc3686ce
Siam <siam48444@gmail.com>
2024-07-05 21:50:15 +0600

    main

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 var tl = gsap.timeline(); // GSAP Timeline
 var mm = gsap.matchMedia(); // GSAP Match-Media
+
+function loaded_animation() {
+    gsap.from("nav", { y: "-100%", duration: 0.5 });
+}
+loaded_animation();
