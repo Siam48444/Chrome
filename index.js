b30079d0abd37e9555c96edcbd77a527a76d60bc
Siam <siam48444@gmail.com>
2024-07-05 11:15:32 +0600

    main

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-window.addEventListener("scroll", () => {
+function jumplink_appear_animation() {
+    fixed_jumplink_container = document.querySelector(".fixed_jumplink_container");
+
     if (document.querySelector("main").getBoundingClientRect().bottom < 0) {
-        gsap.to("nav", { y: "-105%" });
-        document.querySelector(".fixed_jumplink_container").classList.add("jumplink_appear");
+        gsap.to("nav", { y: "-105%", duration: 0.3 });
+        fixed_jumplink_container.classList.add("jumplink_appear");
     } else {
-        gsap.to("nav", { y: 0 });
-        document.querySelector(".fixed_jumplink_container").classList.remove("jumplink_appear");
+        gsap.to("nav", { y: 0, duration: 0.3 });
+        fixed_jumplink_container.classList.remove("jumplink_appear");
     }
-});
+}
+window.addEventListener("scroll", () => jumplink_appear_animation());
