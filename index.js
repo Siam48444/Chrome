2aa4b82fc14cba0f6d38bde22a4284f63aae430d
Siam <siam48444@gmail.com>
2024-07-05 17:29:05 +0600

    main

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const main = document.querySelector("main");
-const nav = document.querySelector("nav");
-const fixed_jumplink_container = document.querySelector(".fixed_jumplink_container");
-
-fixed_jumplink_container.style.opacity = 0;
-fixed_jumplink_container.style.scale = 0.9;
-fixed_jumplink_container.style.pointerEvents = "none";
-
 function jumplink_appear_animation() {
+    const main = document.querySelector("main");
+    const nav = document.querySelector("nav");
+    const fixed_jumplink_container = document.querySelector(".fixed_jumplink_container");
+
     function jumplink_appear() {
         gsap.to(nav, { y: "-105%", duration: 0.3 });
         gsap.to(fixed_jumplink_container, { opacity: 1, scale: 1, pointerEvents: "all", duration: 0.3 });
@@ -20,3 +16,5 @@ function jumplink_appear_animation() {
 }
 
 window.addEventListener("scroll", () => jumplink_appear_animation());
+
+gsap.to(".hero_images_wrapper", { x: "-100%", scrollTrigger: { trigger: "main", start: "top 0", scrub: 1 } });
