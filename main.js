c63bc215be4af7997002a655781138dffc01410b
Siam <siam48444@gmail.com>
2024-07-06 10:57:25 +0600

    main

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ var mm = gsap.matchMedia(); // GSAP Match-Media
 
 function loaded_animation() {
     gsap.from("nav", { y: "-100%", duration: 0.5 });
+    gsap.from(".hero_images_wrapper img", { opacity: 0, scale: 0.9, y: "20%", duration: 0.5 });
 }
 loaded_animation();
