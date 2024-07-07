7c2db0c7400db8b795d68a643056ce415ac0873d
Siam <siam48444@gmail.com>
2024-07-07 18:15:22 +0600

    main

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function loaded_animation() {
     gsap.from(".hero_images_wrapper img", { opacity: 0, scale: 0.9, y: "20%", duration: 0.5 });
 }
 loaded_animation();
+
+function update_section_logo_moves() {
+    gsap.from(".update_visual svg", {
+        left: 0,
+        duration: 1,
+        delay: 0.5,
+        rotate: -90,
+        opacity: 0,
+        ease: "expo.out",
+        scrollTrigger: { trigger: ".update_visual svg" , start: "top 90%"},
+    });
+}
+update_section_logo_moves();
